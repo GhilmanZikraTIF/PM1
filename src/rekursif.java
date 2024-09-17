9a71ca3894be2e99b85844ce44fc1e89979a2b4d
Ghilman Zikra <12350111606@students.uin-suska.ac.id>
2024-09-17 15:11:06 +0700

    latihan rekursif java

--- src/rekursif.java
+++ src/rekursif.java
@@ -1,15 +1,15 @@
 public class rekursif {
     public static void main(String[] args) {
-        System.out.println("rekursif");
+        System.out.println(factorial (5));
+    }
 
-        public int faktorial (int i){
-            if (i<0){
-                return 0;
-            }
-            if (i==1 || i==0 ){
-                return 1;
-            }
-            return i * faktorial (i -1);
+    public static int factorial(int i){
+        if (i < 0){
+            return 1;
+        }else if (i == 0){
+            return 1;
+        }else {
+            return i * factorial(i - 1);
         }
     }
 }
